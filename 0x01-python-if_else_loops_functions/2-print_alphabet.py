111bef62b389de3f18c54f817990bf28288ac157
semi <semilore@omotade-michaels.com>
2023-03-07 17:02:46 +0100

    debug tests

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ap in range(97, 122):
+for ap in range(97, 123):
     print("{}".format(chr(ap)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for ap in range(97, 122):
-    if chr(ap) == 'q' or chr(ap) == 'e':
-        continue
-    print("{}".format(chr(ap)), end="")
+    if chr(ap) != 'q' and chr(ap) != 'e':
+        print("{}".format(chr(ap)), end="")
