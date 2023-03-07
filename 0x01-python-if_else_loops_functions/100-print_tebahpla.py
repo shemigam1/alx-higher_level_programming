56ab7c9fa702330b202bcbebb1d6cd5b696c904d
semi <semilore@omotade-michaels.com>
2023-03-07 16:39:44 +0100

    test 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 i = 0
 for ap in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(ap - i)), end="")
-    i = 32 if i == 0 else 0
+    print("{}{}".format(chr(ap), chr(ap - 33), end="")
