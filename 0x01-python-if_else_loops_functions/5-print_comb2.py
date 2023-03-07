b97f7616ea9da48d7409ae2e62d5245c9ef9cca7
semi <semilore@omotade-michaels.com>
2023-03-07 16:17:38 +0100

    test 4

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    if num != 99:
+    if num != 99: 
         print("{}".format(num:02), end=", ")
-    else:
+    else: 
         print("{}".format(num:02))
