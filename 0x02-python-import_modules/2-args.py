34316ad190d22d5c189acde9ab46c3a04ed8f391
semi <semilore@omotade-michaels.com>
2023-03-09 20:07:10 +0100

    mandatory tasks corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    if len(argv) < 2:
+    count = len(argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif len(argv) == 2:
+    elif count == 1:
         print("1 argument:")
-        print("1: {}".format(argv[1]))
     else:
-        print("{} arguments:".format(int(len(argv) - 1)))
-        for arg in range(1, len(argv)):
-            print("{}: {}".format(arg, int(argv[arg])))
+        print("{} arguments:".format(count))
+    for arg in range(count):
+            print("{}: {}".format(arg + 1, argv[arg + 1]))
