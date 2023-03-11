f77898118784fe74ac5b8f728b25ef0245bbfcd8
semi <semilore@omotade-michaels.com>
2023-03-11 22:13:18 +0100

    task 5: print a matrix of ints

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in row:
-            print("{}".format(i), end=" ")
-        print('\n')
+            print("{:d}".format(i), end=" ")
+        print()
