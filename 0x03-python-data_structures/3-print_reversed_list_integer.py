7f8ac9d2418b1bb9ec858ab094c8bdec398c4447
semi <semilore@omotade-michaels.com>
2023-03-11 21:34:15 +0100

    task 3: print members of a list in reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    rev_list = my_list[::-1]
-    for list in range(len(my_list)):
-        print("{:d}".format(rev_list[list]))
+    if isinstance(my_list, list):
+        rev_list = my_list[::-1]
+        for list in range(len(my_list)):
+            print("{:d}".format(rev_list[list]))
