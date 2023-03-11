a99618f4db83ad2858031388ea87606642dfbf1c
semi <semilore@omotade-michaels.com>
2023-03-11 21:57:07 +0100

    task 5: remove c and C in a sentence and print new sentence

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    copy = [i for i in my_string if i != 'c' and i != 'C']
-    print("".join(copy))
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            print(i, end="")
