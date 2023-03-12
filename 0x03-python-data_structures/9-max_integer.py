2be87b625bb4432dca6a01b3e30ce1f1b68e7884
semi <semilore@omotade-michaels.com>
2023-03-12 20:07:44 +0100

    task 9: finf max number in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
-    max = 0
+    i = 0
+    max = my_list[i]
     for num in my_list:
         if num > max:
             max = num
