ee16e5341bb4c04dbbbacdc5ec2a37169591fbbc
semi <semilore@omotade-michaels.com>
2023-03-12 19:54:49 +0100

    task 8:

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,4 @@
 def multiple_returns(sentence):
     if sentence == "":
-        return None
-    length = len(sentence)
-    first = tup[0]
-    return length, first
+        return (0, None)
+    return (len(sentence), sentence[0])
