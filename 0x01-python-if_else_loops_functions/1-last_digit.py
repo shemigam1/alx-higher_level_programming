877ab6540077bde968d350142cdf8c78c1e579c3
semilore <semilore@omotade-michaels.com>
2023-08-08 19:51:25 +0100

    fix: check last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
+last = abs(number) % 10
 if number < 0:
-    number = number * -1
-    last = last * -1
+    last = -last
 end = ""
 if last > 5:
     end = "and is greater than 5"
