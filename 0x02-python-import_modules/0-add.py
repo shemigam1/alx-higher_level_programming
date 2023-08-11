f47eee6d5d45ec88378cf49668b0e9fba5e800bd
semilore <semilore@omotade-michaels.com>
2023-08-11 09:14:30 +0100

    add: import add function and used it

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-res = add(a, b)
-print("{} + {} = {}".format(a, b, res))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    from add_0 import add
+    res = add(a, b)
+    print("{} + {} = {}".format(a, b, res))
