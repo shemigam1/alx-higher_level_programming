2203e2b210fdb20d7edbc6892511235cdb4c1866
semilore <semilore@omotade-michaels.com>
2023-08-28 19:49:39 +0100

    prints elements of a list safely

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    index = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            index += 1
+    except:
+        pass
+    finally:
+        print()
+    return index
