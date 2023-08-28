e5e98f8e0f0d30cf5cbe83ae348fc4c441aa3601
Oluwasemilore Omotade-Michaels <97962478+shemigam1@users.noreply.github.com>
2023-08-28 19:27:10 +0000

    fix:

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     index = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end="")
             index += 1
-    except IndexError:
-        break
+        except IndexError:
+            break
     print("")
     return index
