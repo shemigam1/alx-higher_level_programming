798decf3cae72dd90de066b8c3eb4d25bf0be467
Oluwasemilore Omotade-Michaels <97962478+shemigam1@users.noreply.github.com>
2023-08-28 19:24:41 +0000

    fix: file permission

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             index += 1
-    except:
-        pass
-    finally:
-        print()
+    except IndexError:
+        break
+    print("")
     return index
