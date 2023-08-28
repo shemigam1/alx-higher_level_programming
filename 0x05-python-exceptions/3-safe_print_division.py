86aed2f9ff6c4baca5101d124bdc304e1c7868e7
semilore <semilore@omotade-michaels.com>
2023-08-28 20:47:49 +0100

    add:

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        ans = a / b
+    except (ZeroDivisionError, TypeError):
+        ans = None
+    finally:
+        print("Inside result: {:d}".format(ans))
+    return ans
