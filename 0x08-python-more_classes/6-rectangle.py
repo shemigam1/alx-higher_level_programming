39481e7e30e9ae260b786e7ccef381204f87ef30
semilore <semilore@omotade-michaels.com>
2023-09-04 15:47:15 -0700

    add: count number of instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,6 +8,7 @@ class Rectangle:
     class Rectangle
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
