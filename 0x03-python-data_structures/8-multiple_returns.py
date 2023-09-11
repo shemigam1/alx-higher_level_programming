97917059c28953eb7797ec1500132ae996643914
semilore <semilore@omotade-michaels.com>
2023-09-11 16:06:05 +0100

    fix:

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence:
-        if sentence == "":
-            sentence[0] = None
         t = len(sentence), sentence[0]
         return t
+    else:
+        return (0, None)
