168cd1326ecdee8f0400841cae5818529d278b68
semilore <semilore@omotade-michaels.com>
2023-09-12 21:40:14 +0100

    add: returns attributes and methods of an obj

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+module contains lookup function
+"""
+
+
 def lookup(obj):
     """
-    Description:  a function that returns the list of available attributes and methods of an object
+    Description:  a function that returns the list of available
+    attributes and methods of an object
     """
     return dir(obj)
