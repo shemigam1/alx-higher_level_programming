86da86f1849495334574d98659547bc110c5fc84
semilore <semilore@omotade-michaels.com>
2023-09-12 21:51:46 +0100

    add: verifies if an object is an instance of a class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,6 +4,10 @@
 
 
 def is_same_class(obj, a_class):
+    """
+    returns True if the object is exactly an instance of
+    the specified class ; otherwise False
+    """
     if type(obj) == a_class:
         return True
     else:
