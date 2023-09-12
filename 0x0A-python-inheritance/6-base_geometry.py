7aded9f26bc4309d3cddb2353fda8b6fe6df4224
semilore <semilore@omotade-michaels.com>
2023-09-12 22:05:43 +0100

    add: empty class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""this module contains class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class basegeometry"""
+    def area(self):
+        """calculates area"""
+        raise Exception("area() is not implemented")
