cd209c582af112d5cde74e98ffe8c5e7a87d15ea
semilore <semilore@omotade-michaels.com>
2023-09-12 22:26:35 +0100

    add: class rectangle that inherits from baseGeometry

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -28,3 +28,11 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """calc area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """string representation of class"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
