17e12111d34268b3e9093ec1d2c906c7dad91362
semilore <semilore@omotade-michaels.com>
2023-09-13 13:34:33 +0100

    fix:

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,3 @@
-"""
-#!/usr/bin/python3
-"""module contains function add_item"""
-import sys
-import os
-
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-
-    filename = "add_item.json"
-    if os.path.isfile(filename):
-        my_list = load_from_json_file(filename)
-    else:
-        my_list = []
-    my_list.extend(sys.argv[1:])
-    save_to_json_file(my_list, "add_item.json")
-"""
-
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
 import sys
