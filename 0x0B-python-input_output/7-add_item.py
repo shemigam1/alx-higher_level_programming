de5029e9c7b649d27c72b1e4625fb9dcb49d2537
semilore <semilore@omotade-michaels.com>
2023-09-13 13:49:50 +0100

    fix:

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,13 +4,13 @@ import sys
 import os
 import json
 
-# Import the custom functions
+"""Import the custom functions"""
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
 
 def main():
-    # Check if the add_item.json file exists, and load its content if it does
+    """Check if the add_item.json file exists, and load its content if it does"""
     if os.path.isfile('add_item.json'):
         my_list = load_from_json_file('add_item.json')
     else:
@@ -24,4 +24,5 @@ def main():
 
 
 if __name__ == "__main__":
+    """random docs"""
     main()
