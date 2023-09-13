9e556694359a535eeee69f7d6bf8972cf94946f2
semilore <semilore@omotade-michaels.com>
2023-09-13 13:54:44 +0100

    fix:

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,10 +5,4 @@
 
 def class_to_json(obj):
     """converts class to json"""
-    serialized_dict = {}
-    attributes = dir(obj)
-    for attr_name in attributes:
-        attr_value = getattr(obj, attr_name)
-        if isinstance(attr_value, (list, dict, str, int, bool)):
-            serialized_dict[attr_name] = attr_value
-    return serialized_dict
+    return obj.__dict__
