3024c49b4f8a71ca4c140748be638f2e52e4db6b
semilore <semilore@omotade-michaels.com>
2023-09-13 10:59:25 +0100

    add: json representation

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+
+"""module contains function def from_json_string(my_str):"""
+
+
+def from_json_string(my_str):
+    """returns an object (Python data structure)
+    represented by a JSON string
+    """
+    return json.loads(my_str)
