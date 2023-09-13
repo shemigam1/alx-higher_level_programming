0b40ca61c897fa7405219fc88bf5e2a981d518e8
semilore <semilore@omotade-michaels.com>
2023-09-13 10:50:03 +0100

    add: converts object to json representaion

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+
+"""this module contains function to_json_string"""
+
+
+def to_json_string(my_obj):
+    """returns json representation of object"""
+    return json.dumps(my_obj)
