1c3cd0e26d234498cff86726aa20f019bb9172eb
semilore <semilore@omotade-michaels.com>
2023-09-13 13:43:25 +0100

    fix:

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+
 import sys
+import os
+import json
+
+# Import the custom functions
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+def main():
+    # Check if the add_item.json file exists, and load its content if it does
+    if os.path.isfile('add_item.json'):
+        my_list = load_from_json_file('add_item.json')
+    else:
+        my_list = []
+
+    # Add command-line arguments to the list
+    my_list.extend(sys.argv[1:])
+
+    # Save the updated list as a JSON representation in add_item.json
+    save_to_json_file(my_list, 'add_item.json')
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+    main()
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+"""module contains function class_to_json"""
+
+
+def class_to_json(obj):
+    """converts class to json"""
+
