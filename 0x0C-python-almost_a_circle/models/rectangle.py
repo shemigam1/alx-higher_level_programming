4adb5b334d4e51f68483b7242853554b064332a6
semilore <semilore@omotade-michaels.com>
2023-09-18 19:57:00 +0100

    add: to dictionary method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -120,3 +120,8 @@ class Rectangle(Base):
                     self.__x = val
                 if key == 'y':
                     self.__y = val
+
+    def to_dictionary(self):
+        """returns dictionary of instance"""
+        return {'id': self.id, 'width': self.__width,
+                'height': self.__height, 'x': self.__x, 'y': self.__y}
