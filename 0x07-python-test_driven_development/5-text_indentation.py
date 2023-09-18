b8710c3ea4c3db8358b5aedd77a25afd87c1c7d8
semilore <semilore@omotade-michaels.com>
2023-09-18 12:56:06 +0100

    add:

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""module contains text_indentation"""
+
+
+def text_indentation(text):
+    """
+    function that prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
