aa5b58c9ff6bd9860c7a0a430648ffd7e91a10b4
semilore <semilore@omotade-michaels.com>
2023-11-06 23:11:29 +0100

    add: to_dictionary method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -149,3 +149,13 @@ class Rectangle(Base):
                     self.__x = val
                 if key == 'y':
                     self.__y = val
+
+    def to_dictionary(self):
+        """returns dictionary representation"""
+        return {
+                "id": self.id,
+                "width": self.__width,
+                "height": self.__height,
+                "x": self.__x,
+                "y": self.__y
+                }
