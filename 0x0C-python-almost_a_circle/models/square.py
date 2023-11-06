f66bc24f55f4691902f568b6c2256debf57f7993
semilore <semilore@omotade-michaels.com>
2023-11-06 19:34:22 +0100

    add: class Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,4 +22,5 @@ class Square(Rectangle):
 
     def __str__(self):
         """str method"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.__x, self.__y, self.__size)
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.__x, self.__y, self.__size)
