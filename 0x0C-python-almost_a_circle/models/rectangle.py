53fee46049af099141e26a9d6e7edc8c4a230372
semilore <semilore@omotade-michaels.com>
2023-11-06 18:14:17 +0100

    add: area method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,3 +95,9 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """
+        area method
+        """
+        return self.__width * self.__height
