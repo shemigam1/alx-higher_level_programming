ad4eebd7cc477da08a7595d9611ba49ea5ff1c09
semilore <semilore@omotade-michaels.com>
2023-11-06 19:31:39 +0100

    add: class Square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+"""
+module contains class Square
+"""
+
+
+from module.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    description: class Rectangle inherits from
+    class Rectangle which inherits from class Base
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """init method"""
+        self.__size = size
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """str method"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.__x, self.__y, self.__size)
