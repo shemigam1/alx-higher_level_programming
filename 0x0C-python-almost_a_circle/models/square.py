333c154707c017f1f05c592a8455e1cef1c89e1f
semilore <semilore@omotade-michaels.com>
2023-11-06 20:02:36 +0100

    add: update class Square getters and setters

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,7 +16,7 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         """init method"""
-        self.__size = size
+        self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
