904ea030939b2ef0bced4e326ee5061594635a25
semilore <semilore@omotade-michaels.com>
2023-11-06 23:25:45 +0100

    add: to_dictionary method in class square

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,6 +25,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         description: returns json representation
