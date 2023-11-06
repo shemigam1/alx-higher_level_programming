9a870eba66bf120008b210b3cd39539a6e1a8e0a
semilore <semilore@omotade-michaels.com>
2023-11-06 17:12:57 +0100

    add: class rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -32,7 +32,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """setter for height"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -46,7 +46,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """setter for height"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
