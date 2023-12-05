ee4541c29c1fc2ce14e17591f4d61f11ed3b1b9e
semilore <semilore@omotade-michaels.com>
2023-12-05 16:41:07 +0100

    fEAT:

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 --created table with uniquee id
-CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `unique_id`(`id` INT DEFAULT 1 UNIQUE, `name` VARCHAR(256));
