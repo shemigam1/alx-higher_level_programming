3af630bfb382a39d672e0bc8cca8e5cf701d5aa2
semilore <semilore@omotade-michaels.com>
2023-12-11 17:09:03 +0100

    style checks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,7 @@ const args = process.argv.length;
 if (args <= 3) {
   console.log(0);
 } else {
-  const arg = process.argv.map(Number).slice(2, args).sort((a, b) => a - b);
-  console.log(arg[args - 2]);
+  const arg = process.argv.slice(2).map(Number);
+  const sorted = arg.sort(function(a, b){return b - a});
+  console.log(sorted[1]);
 }
