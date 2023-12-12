106bbe03b279647c535858ab438bf5a3190e32f3
semilore <semilore@omotade-michaels.com>
2023-12-12 22:07:15 +0100

    advanced solutions

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const list = require('./100-data.js').list;
+const list = require('./100-data.js');
 const newList = list.map((item, index) => item * index);
 console.log(list);
 console.log(newList);
--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const dict = require('./101-data.js').dict;
+const dict = require('./101-data.js');
 const newDict = {};
 
 Object.getOwnPropertyNames(dict).forEach(occurences => {
