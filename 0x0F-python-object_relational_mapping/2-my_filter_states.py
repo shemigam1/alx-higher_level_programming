b9ac8e240c88ca74d4a459ea871737436a690153
semilore <semilore@omotade-michaels.com>
2023-12-18 16:56:21 +0100

    fix: match args

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute(
-            "SELECT * FROM states WHERE name = {} \
+            "SELECT * FROM states WHERE name = '{}' \
                     ORDER BY states.id ASC".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
