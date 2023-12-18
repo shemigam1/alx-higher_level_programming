1211c28c17f250dbe456265f6071958a96b4e4a7
semilore <semilore@omotade-michaels.com>
2023-12-18 15:30:42 +0100

    feat: connect to db

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,9 @@ if __name == '__main__':
     """
     access db to get states table
     """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+            host="localhost", user=argv[1], port=3306,
+            passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
