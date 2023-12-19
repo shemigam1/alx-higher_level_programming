f01e19946ea35047e4bc73aba588daf12704ff41
semilore <semilore@omotade-michaels.com>
2023-12-19 20:36:40 +0100

    fix:

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,11 +15,11 @@ if __name__ == '__main__':
             host="localhost", user=argv[1],
             passwd=argv[2], db=argv[3], port=3306)
 
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name \
-                    FROM cities JOIN states ON cities.states_id = states.id \
-                    ORDER BY cities.id ASC")
-    rows = cur.fetchall()
+    with db.cursor() as db_cursor:
+        db_cursor.execute("SELECT cities.id, cities.name, states.name \
+                                FROM cities JOIN states ON cities.state_id \
+                                = states.id ORDER BY cities.id ASC")
+        rows = db_cursor.fetchall()
     if rows is not None:
         for row in rows:
             print(row)
