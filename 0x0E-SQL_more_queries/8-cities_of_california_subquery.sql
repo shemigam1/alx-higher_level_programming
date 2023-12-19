3bbf44fcdc09fc6b958723159f9f3356a63f9560
semilore <semilore@omotade-michaels.com>
2023-12-19 14:46:58 +0100

    fix: task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,2 @@
 --lists cities of california in hbtn_0d_usa
-SELECT id, name
-FROM cities
-WHERE state_id
-IN (SELECT id FROM states WHERE name = "California")
-ORDER BY id;
+SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = "California") ORDER BY id;
