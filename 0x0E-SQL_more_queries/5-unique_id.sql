1c2cf3dbe45dda7a6a570e050a5c88a5f4de76c0
semilore <semilore@omotade-michaels.com>
2023-12-19 14:44:09 +0100

    fix: task 4, 5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 --created table with uniquee id
-CREATE TABLE IF NOT EXISTS `unique_id`(`id` INT DEFAULT 1 UNIQUE, `name` VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT UNIQUE DEFAULT 1, name VARCHAR(256));
