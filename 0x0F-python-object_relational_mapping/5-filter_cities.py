f27f9217d0a1af09bcbd33b239ded2ad94459805
semilore <semilore@omotade-michaels.com>
2023-12-20 14:07:24 +0100

    fix:

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,7 +2,8 @@
 
 
 """
-takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa
+takes in the name of a state as an argument and lists
+all cities of that state, using the database hbtn_0e_4_usa
 """
 
 import MySQLdb
@@ -12,7 +13,8 @@ if __name__ == '__main__':
     """
     dont execute on import
     """
-    db = MySQLdb.connect(host="localhost", user=argv[1],
+    db = MySQLdb.connect(
+            host="localhost", user=argv[1],
             passwd=argv[2], db=argv[3], port=3306)
     with db.cursor() as db_cursor:
         db_cursor.execute("""
