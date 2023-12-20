42756f613689ea2ac6b7f8d91a682e3010d47b5d
semilore <semilore@omotade-michaels.com>
2023-12-20 14:04:39 +0100

    fix:

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     dont execute on import
     """
     db = MySQLdb.connect(host="localhost", user=argv[1],
-            passwd=argv[2], db=argv[3], port=3306))
+            passwd=argv[2], db=argv[3], port=3306)
     with db.cursor() as db_cursor:
         db_cursor.execute("""
         SELECT cities.id, cities.name
