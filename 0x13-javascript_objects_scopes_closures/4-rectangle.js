4f345cfd060aaab4d211e2d73e3ca7a68ad7f84b
semilore <semilore@omotade-michaels.com>
2024-02-13 20:32:27 +0100

    closures

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,1 @@
+javascript objects and closures
