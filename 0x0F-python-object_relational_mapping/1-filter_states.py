41578ef84b5641c04b30c58bae1fc962702664d9
semilore <semilore@omotade-michaels.com>
2024-02-18 05:33:40 +0100

    fix: syntax

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
             host="localhost", user=argv[1], port=3306,
             passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("
-            SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+    cur.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
                     ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
