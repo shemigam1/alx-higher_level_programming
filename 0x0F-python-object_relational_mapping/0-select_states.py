04398e28967ae36b190854ca4b593debc2a17a42
semilore <semilore@omotade-michaels.com>
2024-02-18 04:34:44 +0100

    test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 
 """
-This script connects to MySQL, with args
-from the cmd line
-argv[1] - mysql username
-argv[2] - mysql password
-argv[3] - database name
+this script takes 3 args from the cmd line
+mysql username
+mysql password
+db name
 """
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
     """
-    connect to db
+    access db to get states table
     """
     db = MySQLdb.connect(
-        host="localhost", user=argv[1], port=3306,
-        passwd=argv[2], db=argv[3])
+            host="localhost", user=argv[1], port=3306,
+            passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
