0c55420823cd075e32128e213645016f1a410958
semilore <semilore@omotade-michaels.com>
2024-02-18 04:29:58 +0100

    feat: connect to db

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This script connects to MySQL, with args
+from the cmd line
+argv[1] - mysql username
+argv[2] - mysql password
+argv[3] - database name
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    connect to db
+    """
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306,
+        passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
