42f958db17f3cb88275f08bf9ed82256e3b39d02
semilore <semilore@omotade-michaels.com>
2024-03-07 20:52:05 +0100

    send data to url

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+sends post request with the email as a parameter, and displays the body of the response (decoded in utf-8)
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.arg[2]
+    data = urllib.parse.urlencode({'email': email}).encode('utf8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        res_data = response.read().decode('utf8')
+    print(res_data)
