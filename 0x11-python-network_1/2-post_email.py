583216bfaa602ef5df3d07fb2517e36a56239a6a
semilore <semilore@omotade-michaels.com>
2024-03-07 20:55:49 +0100

    fix: argv

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    email = sys.arg[2]
+    email = sys.argv[2]
     data = urllib.parse.urlencode({'email': email}).encode('utf8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
