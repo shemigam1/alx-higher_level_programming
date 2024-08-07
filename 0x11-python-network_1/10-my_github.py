e17f3fc873f9f888a67d7f44d352524dfd7a7929
semilore <semilore@omotade-michaels.com>
2024-08-07 22:57:39 +0100

    rebase

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     name = sys.argv[1]
     pwd = "Bearer {}".format(sys.argv[2])
     url = 'https://api.github.com/user'
-    headers = {"Accept": "application/vnd.github+json", "Authorization": pwd, "X-GitHub-Api-Version": "2022-11-28"}
+    headers = {"Accept": "application/vnd.github+json",
+               "Authorization": pwd, "X-GitHub-Api-Version": "2022-11-28"}
     r = requests.get(url, headers=headers)
     res = r.json()
     # print(res)
