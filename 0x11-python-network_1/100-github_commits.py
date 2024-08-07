fecee940a9752c33f0ef4b9982bd5778514024eb
semilore <semilore@omotade-michaels.com>
2024-08-07 23:45:22 +0100

    add: github

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+post search api param
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    name = sys.argv[1]
+    repo = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(name, repo)
+    r = requests.get(url)
+    res = r.json()
+    # print(res)
+    for i in range(10):
+        print('{}: {}'.format(
+            res[i].get("sha"),
+            res[i].get("commit").get("author").get("name")))
